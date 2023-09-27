daf0f90fc3c1ba8fd0f1a3f990f1a9df225fc0a3
Kirat Bains <kiratbains09@gmail.com>
2023-09-26 21:45:48 -0700

    feat: main py update

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from forex_python.converter import CurrencyRates
+
+app = Flask(__name__)
+c = CurrencyRates()
+
+# Route to convert currency
+@app.route('/convert', methods=['GET'])
+def convert_currency():
+    try:
+        from_currency = request.args.get('from')
+        to_currency = request.args.get('to')
+        amount = float(request.args.get('amount'))
+
+        if not from_currency or not to_currency or not amount:
+            return jsonify({"error": "Invalid input"}), 400
+
+        conversion = c.convert(from_currency, to_currency, amount)
+        result = {
+            "from": from_currency,
+            "to": to_currency,
+            "amount": amount,
+            "converted_amount": conversion,
+        }
+
+        return jsonify(result), 200
+
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
